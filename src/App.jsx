20fd5b05e7402401c4a5c850bbdeb205ba1d79fa
V1cT0rY23 <chibuezedc4@gmail.com>
2025-05-15 15:47:44 +0100

    routing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useEffect } from 'react'
-import Home from './Home.jsx';
-import Vehicles from './Vehicles.jsx';
-import About from './About.jsx';
-import Contacts from './contact.jsx';
+import { useEffect } from "react";
+import Home from "./Home.jsx";
+import Vehicles from "./Vehicles.jsx";
+import About from "./About.jsx";
+import Contacts from "./contact.jsx";
 import { MyProvider } from "./provider/provider.jsx";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { StyledEngineProvider } from "@mui/material/styles";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-
 
 function App() {
   useEffect(() => {
@@ -19,18 +17,19 @@ function App() {
     });
   }, []);
   return (
-      
-    <StyledEngineProvider injectFirst>
-    <MyProvider>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/vehicles" element={<Vehicles />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contacts />} />
-      </Routes>
-    </MyProvider>
-  </StyledEngineProvider>
+    <BrowserRouter>
+      <StyledEngineProvider injectFirst>
+        <MyProvider>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/vehicles" element={<Vehicles />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contacts />} />
+          </Routes>
+        </MyProvider>
+      </StyledEngineProvider>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
